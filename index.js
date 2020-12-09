491de8e741aa62e596d54d2e22a681d65a7aa542
bebofofum <bri.boehm@gmail.com>
2020-12-09 16:27:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
+const random = Math.random() * 10;
+
+const mod = 34 % 5;
+
+const max = Math.max(1, 2, 3, 12, 20);
+console.log(max)
 // Write your code here
